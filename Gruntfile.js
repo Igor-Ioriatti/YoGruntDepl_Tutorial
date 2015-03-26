43da4721d9028c440bead97fb010e35187b38112
igor <igor.ioriatti@hotmail.com>
2015-03-26 03:22:17 +0100

    xxx

--- Gruntfile.js
+++ Gruntfile.js
@@ -342,7 +342,7 @@ module.exports = function (grunt) {
         heroku: {
           options: {
             remote: 'git@heroku.com:warm-brook-5328.git',
-            branch: 'master'
+            branch: 'master',
             tag: pkg.version
           }
         },
